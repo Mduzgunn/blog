4cdeb62ebe4686d3fc0d2fa1e0fa1968c293ac88
Apocalypse <aduzgun99@gmail.com>
2021-10-14 00:31:17 +0300

    exercise

--- src/main/java/com/md/blog/BlogApplication.java
+++ src/main/java/com/md/blog/BlogApplication.java
@@ -1,13 +1,17 @@
 package com.md.blog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
+    public void run(String... args) throws Exception {
+        System.out.println("deneme");
+    }
 }
