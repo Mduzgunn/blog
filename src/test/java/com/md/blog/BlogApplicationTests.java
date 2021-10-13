efc1bc801fdf405dec9e606f16adeb0a636ffb2f
Apocalypse <aduzgun99@gmail.com>
2021-10-14 00:28:10 +0300

    exercise

---
+++ src/test/java/com/md/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.md.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
