64cc76a420934a2f97abc6b041b2e5f4d44b8014
Apocalypse <aduzgun99@gmail.com>
2021-11-17 17:01:58 +0300

    bug fix

--- src/main/java/com/md/blog/config/ExampleData.java
+++ src/main/java/com/md/blog/config/ExampleData.java
@@ -1,48 +1,46 @@
-//package com.md.blog.config;
-//
-//import com.md.blog.model.Post;
-//import com.md.blog.model.PostTags;
-//import com.md.blog.repository.CommentRepository;
-//import com.md.blog.repository.PostRepository;
-//import com.md.blog.repository.UserRepository;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-//import org.springframework.stereotype.Component;
-//
-//import com.md.blog.model.User;
-//
-//import java.util.Collections;
-//
-//@Component
-//@ConditionalOnProperty(name = "command.line.runner.enable",havingValue = "true")
-//public class ExampleData implements CommandLineRunner {
-//    private final UserRepository userRepository;
-//    private final PostRepository postRepository;
-//    private final CommentRepository commentRepository;
-//
-//    public ExampleData(UserRepository userRepository, PostRepository postRepository, CommentRepository commentRepository) {
-//        this.userRepository = userRepository;
-//        this.postRepository = postRepository;
-//        this.commentRepository = commentRepository;
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//
-//        User user1 = new User(
-//                "ugurcandede",
-//                "ugur@dede.com",
-//                "Ugurcan Dede",
-//                Collections.emptyList(),
-//                Collections.emptyList());
-//
-//        Post post = new Post(
-//                "Hello",
-//                "Hello Folksie!~",
-//                PostTags.CODE,
-//                user1);
-//
-//
-//    }
-//}
+package com.md.blog.config;
+
+import com.md.blog.model.Post;
+import com.md.blog.model.PostTags;
+import com.md.blog.repository.CommentRepository;
+import com.md.blog.repository.PostRepository;
+import com.md.blog.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import com.md.blog.model.User;
+
+import java.util.Collections;
+
+@Component
+@ConditionalOnProperty(name = "command.line.runner.enable",havingValue = "true")
+public class ExampleData implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+
+    public ExampleData(UserRepository userRepository, PostRepository postRepository, CommentRepository commentRepository) {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        /* This line will provide more reliable Data loading process on booting of application */
+        if (!userRepository.findAll().isEmpty()) {
+            return;
+        }
+
+    User user= new User(
+            "username",
+            "user@gmail.com",
+            Collections.emptyList(),
+            Collections.emptyList()
+    );
+    userRepository.save(user);
+
+    }
+}
